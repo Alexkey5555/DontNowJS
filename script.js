9dea4d5f375498129f78a718077eb2f6e8c0b79a
Alexandr Yeremieiev <sanya@hackermail.com>
2021-12-09 23:33:35 +0100

    поправил жирный шрифт

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ books[0].before(books[1])
 books[5].after(books[2])
 books[0].after(books[4])
 document.body.style.backgroundImage = 'url(./image/book.jpg)';
-link[0].innerHTML = 'Книга 3. this и Прототипы Объектов';
+link[0].style.fontWeight = 'normal';
+link[0].innerHTML = 'Книга 3. this и <b>Прототипы</b> Объектов';
 banner.remove()
 listBook2[9].after(listBook2[2])
 listBook2[3].after(listBook2[6])
@@ -19,7 +20,7 @@ listBook5[1].after(listBook5[9])
 listBook5[9].after(listBook5[3])
 listBook5[3].after(listBook5[4])
 listBook5[7].after(listBook5[5])
-part8.textContent = 'Глава 8: За пределами ES6';
+part8.innerHTML = '<b>Глава 8: За пределами ES6</b>';
 listBook6[8].append(part8);
 
 
